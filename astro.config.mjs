48ef5056226af385d1345bb325c6385217b8e988
Mike Alberts <mikealberts@gmail.com>
2023-10-08 16:35:27 -0400

    New config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+
+  build: {
+    assets: '_compiled'
+  }
 });
