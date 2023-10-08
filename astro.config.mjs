aeaefe4f3b185b16dd503808219306dc0e651e37
Mike Alberts <mikealberts@gmail.com>
2023-10-08 14:10:13 -0400

    Add .env files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dawebmastaa.github.io',
   outDir: './docs',
   integrations: [tailwind()]
 });
