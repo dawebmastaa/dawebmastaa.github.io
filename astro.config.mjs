d8628dc4677c890ca5a236d59b2293a49bbecc5e
Mike Alberts <mikealberts@gmail.com>
2023-10-08 13:44:04 -0400

    SOS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dawebmastaa.github.io',
   outDir: './docs',
   integrations: [tailwind()]
 });
