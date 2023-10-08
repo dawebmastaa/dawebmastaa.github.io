f4fce38f16bfd5819775823645de848557d77170
Mike Alberts <mikealberts@gmail.com>
2023-10-07 21:06:24 -0400

    Another build

--- docs/_astro/hoisted.50a3ac05.js
+++
@@ -1,1 +0,0 @@
-import"https://unpkg.com/flowbite@1.8.1/dist/flowbite.js";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'media',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    "./node_modules/flowbite/**/*.js"
+  ],
 	theme: {
     extend: {
       colors: {
